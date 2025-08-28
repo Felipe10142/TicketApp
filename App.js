87ba319c624a8bb359bf581ea8e53665ae171b34
Felipe Patricio Albornoz (Felipe10142) <felipe_albornoz@estudante.sesisenai.org.br>
2025-08-28 08:46:59 -0300

    feat: Sofrendo por que não consigo fazer que funcione :((((

---
+++ App.js
@@ -0,0 +1,10 @@
+//placeholder temporal para ver si la aplicacion se lanza sin problemas en Android Studio. Con un simple "Oi, Mundo!" me basta.
+import React from 'react';
+import { Text, View } from 'react-native';
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Oi, Mundo!</Text>
+    </View>
+  );
+}
